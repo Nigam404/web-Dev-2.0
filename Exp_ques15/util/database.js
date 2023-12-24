686f6f587346cbdba8776f6b7ebe40fac0becc05
Nigam Kalpataru Chinhara <84139363+Nigam404@users.noreply.github.com>
2023-12-24 13:30:29 +0530

    Update database.js

--- Exp_ques15/util/database.js
+++ Exp_ques15/util/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "root",
-  user: "root",
-  database: "node_complete",
-  password: "Nigam@12345",
+  host: "localhost",
+  user: "root",  //username of Mysql workbench
+  database: "node_complete", //database name  
+  password: "Nigam@12345", //password of our workbench
 });
 
 module.exports = pool.promise();
